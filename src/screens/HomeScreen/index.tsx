96b76a1279694394d55c5a48a9d76e61934111c1
Isaac Almanza <isaac.almanza19@gmail.com>
2024-11-10 22:05:54 -0500

    feat: remove old interceptor

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,13 +2,10 @@ import Balance from "@/components/Balance";
 import OrdersListCard from "@/components/Cards/OrdersListCard";
 import VerifyCtaCard from "@/components/Cards/VerifyCtaCard";
 import LoggedLayout from "@/components/LoggedLayout";
-import { useAuthInterceptor } from "@/hooks/apiInterceptor";
 import { View } from "react-native";
 import HomeActions from "./components/HomeActions";
 
 const HomeScreen = () => {
-  useAuthInterceptor();
-
   return (
     <LoggedLayout>
       <View className="pt-12 pb-6">
