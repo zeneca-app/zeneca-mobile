d7df119dccd2beaa76ab68986c52b5ba19ec646d
Isaac Almanza <isaac.almanza19@gmail.com>
2024-11-30 16:17:22 -0500

    feat: add reset to user store

--- src/storage/userStore.ts
+++ src/storage/userStore.ts
@@ -1,12 +1,14 @@
-import { DBUser } from "@/storage/interfaces";
+import { UserRead } from "@/client";
 import { create } from "zustand";
 
 type UserStore = {
-  user?: DBUser;
-  setUser: (user?: DBUser) => void;
+  user?: UserRead;
+  setUser: (user?: UserRead) => void;
+  resetUser: () => void;
 };
 
 export const useUserStore = create<UserStore>((set) => ({
   user: undefined,
   setUser: (user) => set({ user }),
+  resetUser: () => set({ user: undefined }),
 }));
